415bc2c0f90aca9e370939669ab4d00f4e0133cc
李言路 <lyl_hit@163.com>
2016-08-12 00:03:33 +0800

    调整了一下广播
    排除了自己广播自己的情况

--- src/blockchain/third/communication/Cast.java
+++ src/blockchain/third/communication/Cast.java
@@ -1,25 +1,25 @@
 package blockchain.third.communication;
 
 import java.io.IOException;
-//import java.net.InetAddress;
+import java.net.InetAddress;
 //import java.net.UnknownHostException;
 //发送口
 public class Cast {
 
 	public static void main(String[] args) throws IOException {
-		String IP="127.0.0.1";
-//		String myIP=InetAddress.getLocalHost().toString();
-		FileCast filec=new FileCast(5000);
-		String filePath="D:/desktop/a.txt";
-		filec.Send(IP, filePath);
-		
+//		String IP="127.0.0.1";
+		String myIP=InetAddress.getLocalHost().toString();
+//		FileCast filec=new FileCast(5000);
+//		String filePath="D:/desktop/a.txt";
+//		filec.Send(IP, filePath);
+//		
 		
 //		UniCast RecordCast = new UniCast(IP, 3000);
 //		RecordCast.Send(myIP+":P2P_RecordCast");
 //		UniCast BlockCast = new UniCast(IP, 3001);
 //		BlockCast.Send(myIP+":my block");
-//		BroadCast IPCast = new BroadCast(4000);
-//		IPCast.Send(myIP+":my IP");
+		BroadCast IPCast = new BroadCast(4000);
+		IPCast.Send(myIP+":my IP");
 //		BroadCast RecordCast2 = new BroadCast(4001);
 //		RecordCast2.Send(myIP+":BroadCast");
 //		BroadCast RealeaseCast = new BroadCast(4002);
